d27259a2cc211ef2ec70a0ff737850a6f7311fd4
igarashi <ryosuke.igarashi@jp.ricoh.com>
2018-09-27 20:28:35 +0900

    PS4コントローラのスティックでarduinoを制御する対応

--- Hands-on/3rd_period/sample01/PS4BT/PS4BT.ino
+++ Hands-on/3rd_period/sample01/PS4BT/PS4BT.ino
@@ -3,9 +3,9 @@
  For more information visit my blog: http://blog.tkjelectronics.dk/ or
  send me an e-mail:  kristianl@tkjelectronics.com
  */
-
 #include <PS4BT.h>
-#include <usbhub.h>
+
+#define Enable_I2C
 
 // Satisfy the IDE, which needs to see the include statment in the ino too.
 #ifdef dobogusinclude
@@ -13,134 +13,76 @@
 #endif
 #include <SPI.h>
 
-USB Usb;
-//USBHub Hub1(&Usb); // Some dongles have a hub inside
-BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
+#ifdef Enable_I2C
+#include <Wire.h>
+// A4(SDA)とA5(SCL)は、I2C通信に使用する
+#endif// Enable_I2C
 
-/* You can create the instance of the PS4BT class in two ways */
-// This will start an inquiry and then pair with the PS4 controller - you only have to do this once
-// You will need to hold down the PS and Share button at the same time, the PS4 controller will then start to blink rapidly indicating that it is in pairing mode
+USB Usb;
+BTD Btd(&Usb);
 PS4BT PS4(&Btd, PAIR);
 
-// After that you can simply create the instance like so and then press the PS button on the device
-//PS4BT PS4(&Btd);
-
-bool printAngle, printTouch;
-uint8_t oldL2Value, oldR2Value;
-
 void setup() {
+// シリアル通信の準備
   Serial.begin(115200);
 #if !defined(__MIPSEL__)
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
 #endif
+
+// USB Host sheildの準備
   if (Usb.Init() == -1) {
-    Serial.print(F("\r\nOSC did not start"));
+    Serial.print(F("OSC did not start\r\n"));
     while (1); // Halt
   }
-  Serial.print(F("\r\nPS4 Bluetooth Library Started"));
+  Serial.print(F("PS4 Bluetooth Library Started\r\n"));
+
+#ifdef Enable_I2C
+// I2Cの準備
+  Wire.begin(); // join i2c bus (address optional for master)
+#endif// Enable_I2C
 }
+
 void loop() {
   Usb.Task();
-
   if (PS4.connected()) {
-    if (PS4.getAnalogHat(LeftHatX) > 137 || PS4.getAnalogHat(LeftHatX) < 117 || PS4.getAnalogHat(LeftHatY) > 137 || PS4.getAnalogHat(LeftHatY) < 117 || PS4.getAnalogHat(RightHatX) > 137 || PS4.getAnalogHat(RightHatX) < 117 || PS4.getAnalogHat(RightHatY) > 137 || PS4.getAnalogHat(RightHatY) < 117) {
-      Serial.print(F("\r\nLeftHatX: "));
-      Serial.print(PS4.getAnalogHat(LeftHatX));
-      Serial.print(F("\tLeftHatY: "));
-      Serial.print(PS4.getAnalogHat(LeftHatY));
-      Serial.print(F("\tRightHatX: "));
-      Serial.print(PS4.getAnalogHat(RightHatX));
-      Serial.print(F("\tRightHatY: "));
-      Serial.print(PS4.getAnalogHat(RightHatY));
+    static char older_L_X = 'A';
+    static char older_L_Y = 'A';
+    static char older_R_X = 'A';
+    static char older_R_Y = 'A';
+    uint8_t L_X = PS4.getAnalogHat(LeftHatX);
+    uint8_t L_Y = PS4.getAnalogHat(LeftHatY);
+    uint8_t R_X = PS4.getAnalogHat(RightHatX);
+    uint8_t R_Y = PS4.getAnalogHat(RightHatY);
+    if((L_X == 0 && L_Y == 0) || (R_X == 0 && R_Y == 0)){ // あり得ない値が来たら無視する
+      return;
     }
-
-    if (PS4.getAnalogButton(L2) || PS4.getAnalogButton(R2)) { // These are the only analog buttons on the PS4 controller
-      Serial.print(F("\r\nL2: "));
-      Serial.print(PS4.getAnalogButton(L2));
-      Serial.print(F("\tR2: "));
-      Serial.print(PS4.getAnalogButton(R2));
+    char newer_L_X = 'A' + map(L_X,0,255,0,14);
+    char newer_L_Y = 'A' + map(L_Y,0,255,0,14);
+    char newer_R_X = 'A' + map(R_X,0,255,0,14);
+    char newer_R_Y = 'A' + map(R_Y,0,255,0,14);
+    if(older_L_X != newer_L_X || older_L_Y != newer_L_Y || older_R_X != newer_R_X || older_R_Y != newer_R_Y){
+      older_L_X = newer_L_X;
+      older_L_Y = newer_L_Y;
+      older_R_X = newer_R_X;
+      older_R_Y = newer_R_Y;
+#ifdef Enable_I2C
+      Wire.beginTransmission(8); //transmit to device #8
+      Wire.write(newer_L_X);
+      Wire.write(newer_L_Y);
+      Wire.write(newer_R_X);
+      Wire.write(newer_R_Y);
+      Wire.endTransmission(); //stop transmitting
+#endif// Enable_I2C
+      Serial.print(newer_L_X);
+      Serial.print(newer_L_Y);
+      Serial.print(newer_R_X);
+      Serial.print(newer_R_Y);
+      Serial.print(F("\r\n"));
     }
-    if (PS4.getAnalogButton(L2) != oldL2Value || PS4.getAnalogButton(R2) != oldR2Value) // Only write value if it's different
-      PS4.setRumbleOn(PS4.getAnalogButton(L2), PS4.getAnalogButton(R2));
-    oldL2Value = PS4.getAnalogButton(L2);
-    oldR2Value = PS4.getAnalogButton(R2);
 
     if (PS4.getButtonClick(PS)) {
-      Serial.print(F("\r\nPS"));
+      Serial.print(F("PS\r\n"));
       PS4.disconnect();
-    }
-    else {
-      if (PS4.getButtonClick(TRIANGLE)) {
-        Serial.print(F("\r\nTraingle"));
-        PS4.setRumbleOn(RumbleLow);
-      }
-      if (PS4.getButtonClick(CIRCLE)) {
-        Serial.print(F("\r\nCircle"));
-        PS4.setRumbleOn(RumbleHigh);
-      }
-      if (PS4.getButtonClick(CROSS)) {
-        Serial.print(F("\r\nCross"));
-        PS4.setLedFlash(10, 10); // Set it to blink rapidly
-      }
-      if (PS4.getButtonClick(SQUARE)) {
-        Serial.print(F("\r\nSquare"));
-        PS4.setLedFlash(0, 0); // Turn off blinking
-      }
-
-      if (PS4.getButtonClick(UP)) {
-        Serial.print(F("\r\nUp"));
-        PS4.setLed(Red);
-      } if (PS4.getButtonClick(RIGHT)) {
-        Serial.print(F("\r\nRight"));
-        PS4.setLed(Blue);
-      } if (PS4.getButtonClick(DOWN)) {
-        Serial.print(F("\r\nDown"));
-        PS4.setLed(Yellow);
-      } if (PS4.getButtonClick(LEFT)) {
-        Serial.print(F("\r\nLeft"));
-        PS4.setLed(Green);
-      }
-
-      if (PS4.getButtonClick(L1))
-        Serial.print(F("\r\nL1"));
-      if (PS4.getButtonClick(L3))
-        Serial.print(F("\r\nL3"));
-      if (PS4.getButtonClick(R1))
-        Serial.print(F("\r\nR1"));
-      if (PS4.getButtonClick(R3))
-        Serial.print(F("\r\nR3"));
-
-      if (PS4.getButtonClick(SHARE))
-        Serial.print(F("\r\nShare"));
-      if (PS4.getButtonClick(OPTIONS)) {
-        Serial.print(F("\r\nOptions"));
-        printAngle = !printAngle;
-      }
-      if (PS4.getButtonClick(TOUCHPAD)) {
-        Serial.print(F("\r\nTouchpad"));
-        printTouch = !printTouch;
-      }
-
-      if (printAngle) { // Print angle calculated using the accelerometer only
-        Serial.print(F("\r\nPitch: "));
-        Serial.print(PS4.getAngle(Pitch));
-        Serial.print(F("\tRoll: "));
-        Serial.print(PS4.getAngle(Roll));
-      }
-
-      if (printTouch) { // Print the x, y coordinates of the touchpad
-        if (PS4.isTouching(0) || PS4.isTouching(1)) // Print newline and carriage return if any of the fingers are touching the touchpad
-          Serial.print(F("\r\n"));
-        for (uint8_t i = 0; i < 2; i++) { // The touchpad track two fingers
-          if (PS4.isTouching(i)) { // Print the position of the finger if it is touching the touchpad
-            Serial.print(F("X")); Serial.print(i + 1); Serial.print(F(": "));
-            Serial.print(PS4.getX(i));
-            Serial.print(F("\tY")); Serial.print(i + 1); Serial.print(F(": "));
-            Serial.print(PS4.getY(i));
-            Serial.print(F("\t"));
-          }
-        }
-      }
     }
   }
 }
--- Hands-on/3rd_period/sample02/slave_receiver/slave_receiver.ino
+++ Hands-on/3rd_period/sample02/slave_receiver/slave_receiver.ino
@@ -1,34 +1,30 @@
-// Wire Slave Receiver
-// by Nicholas Zambetti <http://www.zambetti.com>
-
-// Demonstrates use of the Wire library
-// Receives data as an I2C/TWI slave device
-// Refer to the "Wire Master Writer" example for use with this
-
-// Created 29 March 2006
-
-// This example code is in the public domain.
-
-
 #include <Wire.h>
+
+char newer_L_X;
+char newer_L_Y;
+char newer_R_X;
+char newer_R_Y;
 
 void setup() {
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);           // start serial for output
+  Serial.begin(115200);
 }
 
 void loop() {
   delay(100);
 }
 
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
 void receiveEvent(int howMany) {
-  while (1 < Wire.available()) { // loop through all but the last
-    char c = Wire.read(); // receive byte as a character
-    Serial.print(c);         // print the character
+  String str = "";
+  while (Wire.available()) {
+    char c = Wire.read();
+    str += String(c);
+    Serial.print(c);
   }
-  int x = Wire.read();    // receive byte as an integer
-  Serial.println(x);         // print the integer
+  Serial.print(F("\r\n"));
+  newer_L_X = str.charAt(0);
+  newer_L_Y = str.charAt(1);
+  newer_R_X = str.charAt(2);
+  newer_R_Y = str.charAt(3);
 }
